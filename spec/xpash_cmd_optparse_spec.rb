69b3c6edf55ca1181c005d50813224f5ec6e3c51
zeero26 <zeero26@gmail.com>
2011-03-22 22:39:57 +0900

    Add spec about default option and minimum args check.

--- spec/xpash_cmd_optparse_spec.rb
+++ spec/xpash_cmd_optparse_spec.rb
@@ -7,31 +7,73 @@ describe XPash::CmdOptionParser do
   end
 
   it "should parse args and should return option Hash." do
-    @foo.foo(%w(foo bar -l)).should == {:long=>true}
+    @foo.foo(%w(foo bar -l)).should == {:long => true}
     @foo.foo(%w(foo bar)).should == {}
-    @foo.foo(%w(foo bar -s)).should == {:s=>true}
-    @foo.foo(%w(foo -l -s bar)).should == {:s=>true, :long=>true}
-    @foo.foo(%w(foo -l -a baz -s)).should == {:s=>true, :long=>true, :arg=>"baz"}
-  end
-  it "with '-h' or '--help' option, should display help message\n\s\s" +
-     "and raise XPash::ReturnSignal, but no SystemExit." do
-    lambda{@foo.foo(%w(foo -a baz -h))}.should raise_error(XPash::ReturnSignal)
-    stdout = read_stdout
-    stdout.should =~ /[^\n]\n\s{4}-h, --help       Show this help message\.\n/
+    @foo.foo(%w(foo bar -s)).should == {:s => true}
+    @foo.foo(%w(foo -l -s bar)).should == {:s => true, :long => true}
+    @foo.foo(%w(foo -l -a baz -s)).should == {:s => true, :long => true, :arg => "baz"}
   end
 
-  it "with '-v' or '--version' option, should not parse valid option" +
+  context "(with '-h' or '--help' option)" do
+    it "should display help message\n\s\s" +
+     "and raise XPash::ReturnSignal, without SystemExit." do
+      lambda{@foo.foo(%w(foo -a baz -h))}.should raise_error(XPash::ReturnSignal)
+      stdout = read_stdout
+      stdout.should =~ /[^\n]\n\s{4}-h, --help       Show this help message\.\n/
+     end
+  end
+
+  context "with '-v' or '--version' option" do
+    it "should not parse valid option" +
      "\n\s\sin default." do
-    lambda{@foo.foo(%w(foo -l -v))}.should raise_error(OptionParser::InvalidOption)
+      lambda{@foo.foo(%w(foo -l -v))}.should raise_error(OptionParser::InvalidOption)
+     end
   end
 
-  it "'#describe' should show message with indent." do
+  context "if min_args is set" do
+    it "should check args length and should raise error with invalid args." do
+      @foo.optp.min_args = 1
+      lambda{@foo.foo}.should raise_error(ArgumentError)
+    end
+  end
+
+  context "if default_opts is set" do
+    it "should use default value if option is not set." do
+      @foo.optp.default_opts = {:s => true}
+
+      @foo.foo(%w(foo bar -l)).should == {:s => true, :long => true}
+      @foo.foo(%w(foo bar)).should == {:s => true}
+
+      @foo.optp.default_opts = {:arg => "foo"}
+
+      @foo.foo(%w(foo bar)).should == {:arg => "foo"}
+      @foo.foo(%w(foo -l bar)).should == {:arg => "foo", :long => true}
+      @foo.foo(%w(foo -l -a baz -s)).should == {:s => true, :long => true, :arg => "baz"}
+    end
+  end
+
+end
+
+describe XPash::CmdOptionParser, "#describe" do
+  before do
+    @foo = Foo.new
+    prepare_stdout
+  end
+
+  it "should show message with indent." do
     lambda{@foo.foo(%w(foo -a baz -h))}.should raise_error(XPash::ReturnSignal)
     stdout = read_stdout
     stdout.should =~ /[^\n]\n\s{4}#{Foo::Description}\n/
   end
+end
 
-  it "'#separate' should show message with indent." do
+describe XPash::CmdOptionParser, "#separate"  do
+  before do
+    @foo = Foo.new
+    prepare_stdout
+  end
+
+  it "should show message with indent." do
     lambda{@foo.foo(%w(foo --help))}.should raise_error(XPash::ReturnSignal)
     stdout = read_stdout
     stdout.should =~ /[^\n]\n\n#{Foo::Separator}\n/
@@ -40,26 +82,25 @@ describe XPash::CmdOptionParser do
 end
 
 class Foo
+  attr_accessor :optp
+
   Description = "This is desribe sample."
   Separator = "Options:"
 
-  def optparse!(args)
-    unless @optp
-      o = XPash::CmdOptionParser.new
-      o.banner = "option parser test class"
-      o.separator Separator
-      o.on("-l", "--long", "Long option")
-      o.on("-s", "Short option")
-      o.on("--arg arg", "With args option")
-      o.separator "Description:"
-      o.describe Description
-      @optp = o
-    end
-    @optp.parse!(args)
+  def initialize
+    o = XPash::CmdOptionParser.new
+    o.banner = "option parser test class"
+    o.separator Separator
+    o.on("-l", "--long", "Long option")
+    o.on("-s", "Short option")
+    o.on("--arg arg", "With args option")
+    o.separator "Description:"
+    o.describe Description
+    @optp = o
   end
 
   def foo(args)
-    opts = optparse!(args)
+    opts = @optp.parse!(args)
   end
 end
 
