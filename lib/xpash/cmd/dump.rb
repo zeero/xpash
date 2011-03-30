47358e66dba920c775ca3d7e23f4acdcaa8fc4cf
zeero26 <zeero26@gmail.com>
2011-03-30 23:46:26 +0900

    Add option help for dump command.(not implemented yet)

--- lib/xpash/cmd/dump.rb
+++ lib/xpash/cmd/dump.rb
@@ -25,10 +25,17 @@ module XPash
 
     def optparse_dump!(args)
       unless @optparses[:dump]
-        o = CmdOptionParser.new
+        o = CmdOptionParser.new(nil, 22)
         o.banner = "Usage: dump [OPTION] [QUERY]"
         o.separator("Options:")
         o.on("-a", "--all", "Show all document.")
+        o.on("-o", "--output FILENAME",
+          "Write output to file instead of the standard output.",
+          "File will be created or overwritten by default.")
+        o.on("--append",
+          "When use with '-o' option, append output",
+          "to the end of the file.",
+          "Without '-o' option, this option make no sense.")
         @optparses[:dump] = o
       end
       opts = @optparses[:dump].parse!(args)
